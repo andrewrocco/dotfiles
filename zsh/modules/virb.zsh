a9209ebcee86ded06c05c1162c140d17bed2abfa
Andy Rossi <aroccorossi@gmail.com>
2013-06-28 11:31:12 -0400

    making these aliases instead

---
+++ zsh/modules/virb.zsh
@@ -0,0 +1,4 @@
+alias sasswatch="sass --watch sass/base.scss:css/base.css sass/ie.scss:css/ie.css --style compact"
+alias hosthack="sudo vi /etc/hosts"
+alias tovirb='cd ~/Documents/Github/virb'
+alias tothemes='cd ~/Documents/Github/virb/virb.com/_assets/_themes'
